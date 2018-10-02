2dfb17233db98d2f72f58317d1ea52124a148216
Damith C. Rajapakse <damith@gmail.com>
2018-10-02 20:04:57 +0800

    add IntPair.java

---
+++ src/IntPair.java
@@ -0,0 +1,25 @@
+public class IntPair {
+    int first;
+    int second;
+
+    public IntPair(int first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public int intDivision() throws Exception {
+        if (second == 0){
+            throw new Exception("Divisor is zero");
+        }
+        return first/second;
+    }
+
+    @Override
+    public String toString() {
+        return first + "," + second;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new IntPair(1,3));
+    }
+}
